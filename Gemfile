01a6a3c90a35e3a659d6aff91d9e033bdea76cab
Elle Meredith <ellemeredith@gmail.com>
2015-07-07 20:35:52 -0400

    rm compass and use just plain sass

--- Gemfile
+++ Gemfile
@@ -1,13 +1,10 @@
-source 'https://rubygems.org'
-ruby '2.0.0'
+source "https://rubygems.org"
+ruby "2.2.0"
 
-# gem 'sass', '3.3.5'
-gem 'compass'
-gem 'haml'
-gem 'sinatra'
+gem "haml"
+gem "sass"
+gem "sinatra"
 
 group :development do
   gem 'foreman'
-  # gem 'debugger'
-  # gem 'heroku'
 end
--- app.rb
+++ app.rb
@@ -1,21 +1,12 @@
-require 'bundler'
+require "bundler"
 Bundler.require(:default)
 
 configure do
-  Compass.configuration do |config|
-    config.project_path = File.dirname(__FILE__)
-    config.sass_dir = 'views'
-    config.images_dir = File.join 'public', 'images'
-  end
-
-  set :sass, Compass.sass_engine_options
-  set :scss, Compass.sass_engine_options
   set :haml, format: :html5
 end
 
 get '/application.css' do
-  content_type 'text/css', charset: 'utf-8'
-  scss :'stylesheets/application'
+  scss :"stylesheets/application"
 end
 
 get '/' do
